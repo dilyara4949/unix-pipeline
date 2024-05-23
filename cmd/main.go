1bb4235e0478b8055fffad7aacb4f0f559cecbd0
dilya <mukhambetovad49@mail.ru>
2024-05-23 14:13:02 +0500

    fix: change alias to command package

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"log"
 
-	app "github.com/dilyara4949/unix-pipeline/internal/command"
+	c "github.com/dilyara4949/unix-pipeline/internal/command"
 )
 
 func main() {
-	cmds, out, err := app.ReadInput()
+	cmds, out, err := c.ReadInput()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	res, err := app.Execute(cmds, out)
+	res, err := c.Execute(cmds, out)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
